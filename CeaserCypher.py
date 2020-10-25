d6250bcdf417ff16ed392257835030d6c9a2cceb
Fraz <fakiwi@gmail.com>
2020-10-25 14:03:21 +1030

    Upload Simple Ceaser Cipher

---
+++ CeaserCypher.py
@@ -0,0 +1,14 @@
+newList = []
+myString = input("String: ")
+CIPHERKEY = int(input("Key: "))
+
+for x in myString:
+    newList.append(ord(x) + CIPHERKEY)
+
+myString = ""
+for x in newList:
+    myString += chr(x)
+
+print(f"New Message: {myString}")
+
+    
