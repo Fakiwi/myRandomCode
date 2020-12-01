83ee589e206aa903f4120defffe10a749a13b380
Fraz <fakiwi@gmail.com>
2020-12-01 12:24:05 +1030

    Add Comments
    Added some comments for clearer intentions

--- CeaserCypher.py
+++ CeaserCypher.py
@@ -2,9 +2,11 @@ newList = []
 myString = input("String: ")
 CIPHERKEY = int(input("Key: "))
 
+# Create list of character ascii values and shift by cipher key value
 for x in myString:
     newList.append(ord(x) + CIPHERKEY)
 
+# Create new string from list of ciphered values.
 myString = ""
 for x in newList:
     myString += chr(x)
